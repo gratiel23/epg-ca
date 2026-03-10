9ec1027cc342a8023794a43ce9e9d76534f1294c
gratiel23 <gratieljob23@hotmail.com>
2026-03-10 09:02:11 -0400

    Update update_epg.py

--- update_epg.py
+++ update_epg.py
@@ -1,23 +1,21 @@
+import urllib.request
 import gzip
-import requests
 
 url = "https://epgshare01.online/epgshare01/epg_ripper_CA1.xml.gz"
 
 # télécharger le fichier
-r = requests.get(url, timeout=60)
-with open("epg.xml.gz", "wb") as f:
-    f.write(r.content)
+urllib.request.urlretrieve(url, "epg.xml.gz")
 
-# décompresser correctement
+# décompresser
 with gzip.open("epg.xml.gz", "rb") as f:
     data = f.read().decode("utf-8", errors="ignore")
 
-# remplacer les noms
+# modifier les noms de chaines
 data = data.replace("CAF ", "CA FR ")
 data = data.replace("CA-BK", "CA EN")
 data = data.replace("CA BK", "CA EN")
 
-# sauvegarder le nouveau EPG
+# sauvegarder
 with open("epg_ca.xml", "w", encoding="utf-8") as f:
     f.write(data)
 
