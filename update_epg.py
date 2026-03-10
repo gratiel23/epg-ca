2ef2252c385ca041bd84978e8eaa798151820448
gratiel23 <gratieljob23@hotmail.com>
2026-03-10 09:10:05 -0400

    Update update_epg.py

--- update_epg.py
+++ update_epg.py
@@ -1,9 +1,12 @@
 import urllib.request
 import gzip
 
-url = "https://epgshare01.online/epgshare01/epg_ripper_CA.xml.gz"
+url = "https://epgshare01.online/epgshare01/epg_ripper_CA1.xml.gz"
 
-req = urllib.request.Request(url, headers={"User-Agent": "Mozilla/5.0"})
+req = urllib.request.Request(
+    url,
+    headers={"User-Agent": "Mozilla/5.0"}
+)
 
 with urllib.request.urlopen(req) as response:
     data = response.read()
@@ -12,10 +15,4 @@ with open("epg.xml.gz", "wb") as f:
     f.write(data)
 
 with gzip.open("epg.xml.gz", "rb") as f:
-    xml = f.read().decode("utf-8", errors="ignore")
-
-xml = xml.replace("CAF ", "CA FR ")
-xml = xml.replace("CA-BK", "CA EN")
-xml = xml.replace("CA BK", "CA EN")
-
-with open("epg_ca.xml", "w", encoding="utf
+    xml = f.read().decode
