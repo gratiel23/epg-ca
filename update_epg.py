c8cb77017f81a02f1614bc293e19670b7f6c565d
gratiel23 <gratieljob23@hotmail.com>
2026-03-10 09:00:16 -0400

    Update update_epg.py

--- update_epg.py
+++ update_epg.py
@@ -3,15 +3,22 @@ import requests
 
 url = "https://epgshare01.online/epgshare01/epg_ripper_CA1.xml.gz"
 
+# télécharger le fichier
 r = requests.get(url, timeout=60)
-open("epg.xml.gz","wb").write(r.content)
+with open("epg.xml.gz", "wb") as f:
+    f.write(r.content)
 
-with gzip.open("epg.xml.gz","rt",encoding="utf8") as f:
-    data = f.read()
+# décompresser correctement
+with gzip.open("epg.xml.gz", "rb") as f:
+    data = f.read().decode("utf-8", errors="ignore")
 
+# remplacer les noms
 data = data.replace("CAF ", "CA FR ")
 data = data.replace("CA-BK", "CA EN")
 data = data.replace("CA BK", "CA EN")
 
-with open("epg_ca.xml","w",encoding="utf8") as f:
+# sauvegarder le nouveau EPG
+with open("epg_ca.xml", "w", encoding="utf-8") as f:
     f.write(data)
+
+print("EPG généré")
